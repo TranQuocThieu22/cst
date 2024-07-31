a22d6aea99d15260bfb371bc83f9b0b2f733ec68
lâm phạm <lamworkspace74@gmail.com>
2024-07-31 11:48:22 +0700

    feat/43598-xay-dung-web-cst

--- ClientApp/projects/web-cscase/src/app/shared/shared.module.ts
+++ ClientApp/projects/web-cscase/src/app/shared/shared.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SafeHtmlPipe } from './safe-html.pipe';
 import { HighlightPipe } from './highlight.pipe';
-import { OrderByPipe } from './order-by.pipe';
+
 
 @NgModule({
-  declarations: [SafeHtmlPipe, HighlightPipe, OrderByPipe],
+  declarations: [SafeHtmlPipe, HighlightPipe],
   imports: [CommonModule],
-  exports: [SafeHtmlPipe, HighlightPipe, OrderByPipe]
+  exports: [SafeHtmlPipe, HighlightPipe]
 })
 export class SharedModule { }
