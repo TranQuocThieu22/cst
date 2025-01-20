63a98706ed4e57a7036aa299bced161d2483520d
huuluan <huuluanofficial@gmail.com>
2025-01-20 16:41:25 +0700

    commit

--- ClientApp/projects/web-cscase/src/app/pages/lam-viec-online/lam-viec-online.component.ts
+++ ClientApp/projects/web-cscase/src/app/pages/lam-viec-online/lam-viec-online.component.ts
@@ -487,7 +487,9 @@ export class LamViecOnlineComponent implements OnInit {
       },
       complete: () => {
         // Your logic for handling the completion event (optional)
-        this.fetchWorkingOnlinesData();
+        this.filter_datefrom = new Date(new Date().getFullYear(), 0, 1).toLocaleDateString('en-GB');
+        this.filter_dateto = new Date().toLocaleDateString('en-GB');
+        this.fetchWorkingOnlinesData(this.convertDateFormat(this.filter_datefrom), this.convertDateFormat(this.filter_dateto));
       }
     });
 
